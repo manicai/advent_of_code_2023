ffa0010574374789e78654dfbe8a9242f433b844
Ian Glover <ian.glover@gmail.com>
2023-12-02 11:51:23 +0000

    Reformat day 1 part 1 explaining to R

--- day01/part1.py
+++ day01/part1.py
@@ -1,12 +1,11 @@
 import sys
 
 path = sys.argv[1]
-with open(path, 'r') as f:
+with open(path, "r") as file:
     total = 0
-    for l in f.readlines():
-        digits = [int(c) for c in l.strip() if c.isdigit()]
+    for line in file.readlines():
+        digits = [int(char) for char in line if char.isdigit()]
         code = digits[0] * 10 + digits[-1]
         total += code
-    
+
     print(total)
-    
