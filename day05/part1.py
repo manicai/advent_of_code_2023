caa12d2769c9df78dfdaee67558ac47aa2e75bcb
Ian Glover <ian.glover@gmail.com>
2023-12-05 07:53:24 +0000

    Day 5 templates

---
+++ day05/part1.py
@@ -0,0 +1,25 @@
+import aoc
+
+import functools
+import itertools
+import math
+import re
+
+class lazy_map:
+    def __init__(self, key_start, value_start, length):
+        self.key_start = key_start
+        self.value_start = value_start
+        self.length = length
+
+    def contains(self, key):
+        pass
+
+    def value_for(self, key):
+        pass
+    
+
+def part1(input):
+    pass
+
+if __name__ == '__main__':
+    aoc.run_script(part1)
