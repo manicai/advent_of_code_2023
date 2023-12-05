288f19e7686031368251ae4f31b687b39685f8af
Ian Glover <ian.glover@gmail.com>
2023-12-05 12:11:59 +0000

    Dumb (time inefficient) "solution" to day 5 part 2

--- day05/part2.py
+++ day05/part2.py
@@ -9,7 +9,24 @@ import re
 
 
 def part2(input):
-    pass
+    seeds = part1.read_seeds(input)
+    maps = dict((m, part1.read_map(m, input)) for m in part1.map_names)
+    # print(seeds)
+    ranges = [(seeds[i - 1], n) for i, n in enumerate(seeds) if i % 2 == 1]
+    best_locations = []
+    for start, length in ranges:
+        best = None
+        for seed in range(start, start + length):
+            steps = part1.trace_maps(seed, maps)
+            location = steps[-1]
+            if best is None or best > location:
+                best = location
+                print(best)
+        print((start, length), " -> ", best)
+        best_locations.append(best)
 
-if __name__ == '__main__':
+    print("Best location =", min(best_locations))
+
+
+if __name__ == "__main__":
     aoc.run_script(part2)
