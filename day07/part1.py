4cf1315cf06f362618e4e486e82aa94afecd0e4b
Ian Glover <ian.glover@gmail.com>
2023-12-07 07:34:43 +0000

    Day 7 templates.

---
+++ day07/part1.py
@@ -0,0 +1,13 @@
+import aoc
+
+import functools
+import itertools
+import math
+import re
+
+
+def part1(input):
+    pass
+
+if __name__ == "__main__":
+    aoc.run_script(part1)
---
+++ day07/part2.py
@@ -0,0 +1,14 @@
+import aoc
+import part1
+
+import functools
+import itertools
+import math
+import re
+
+
+def part2(input):
+    pass
+
+if __name__ == "__main__":
+    aoc.run_script(part2)
