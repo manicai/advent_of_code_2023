5db869ef0720d2ca5a1962cba3bfcc8a6a24f172
Ian Glover <ian.glover@gmail.com>
2023-12-10 13:19:32 +0000

    Day 10 Part 2

--- day10/part2.py
+++ day10/part2.py
@@ -1,5 +1,5 @@
 import aoc
-import part1
+from part1 import *
 
 import functools
 import itertools
@@ -8,9 +8,116 @@ import numpy
 import re
 
 
-def part2(input):
-    pass
+def trace_route(input):
+    last_position = find_start(input)
+    cell = infer_start_cell(input, last_position)
+    # Doesn't matter which way around the grid we go
+    position = move(last_position, neighbours[cell][0])
+    count = 1
+    # Rather than going both ways we are going all the
+    # way round then halving the distance.
+    route = [last_position]
+    while at(input, position) != "S":
+        steps = neighbours[at(input, position)]
+        cells = [move(position, step) for step in steps]
+        assert last_position in cells
+        new = [c for c in cells if c != last_position]
+        assert len(new) == 1
+        last_position = position
+        route.append(position)
+        position = new[0]
+        count += 1
+        # print(at(input, position))
 
+    return route
+
+
+def scrub_map(input, route):
+    return [
+        "".join(cell if (row, col) in route else "." for col, cell in enumerate(line))
+        for row, line in enumerate(input)
+    ]
+
+
+def specify_start(input):
+    start = find_start(input)
+    cell = infer_start_cell(input, start)
+    return [r.replace("S", cell) for r in input]
+
+
+def print_map(grid):
+    for row in grid:
+        print(row)
+
+
+expansions = {
+    ".": ["...", "...", "..."],
+    "-": ["...", "---", "..."],
+    "|": [".|.", ".|.", ".|."],
+    "L": [".|.", ".L-", "..."],
+    "F": ["...", ".F-", ".|."],
+    "J": [".|.", "-J.", "..."],
+    "7": ["...", "-7.", ".|."],
+}
+
+
+def expand_map(grid):
+    result = []
+    for row in grid:
+        above = []
+        centre = []
+        below = []
+        for cell in row:
+            above.append(expansions[cell][0])
+            centre.append(expansions[cell][1])
+            below.append(expansions[cell][2])
+        result.extend(["".join(above), "".join(centre), "".join(below)])
+    return result
+
+
+def shrink_map(expanded):
+    return [
+        "".join(c for (j, c) in enumerate(r) if j % 3 == 1)
+        for (i, r) in enumerate(expanded)
+        if i % 3 == 1
+    ]
+
+
+def paint(grid):
+    mutable = [list(row) for row in grid]
+    indices = [(0,0),
+                (0, len(grid) - 1),
+                (len(grid) - 1, 0), 
+            (len(grid) - 1, len(grid) - 1)]
+    while len(indices) > 0:
+        row, col = indices.pop()
+        assert mutable[row][col] in ".*"
+        if mutable[row][col] == ".":
+            mutable[row][col] = "*"
+            for d_r, d_c in DIRECTIONS:
+                r = row + d_r
+                c = col + d_c
+                try:
+                    if mutable[r][c] == ".":
+                        indices.append((r, c))
+                except IndexError:
+                    pass  # Off the edge
+            #print_map(["".join(r) for r in mutable])
+            #print()
+    return ["".join(r) for r in mutable]
+
+
+def part2(input):
+    route = trace_route(input)
+    clean = scrub_map(input, route)
+    specific = specify_start(clean)
+    expanded = expand_map(specific)
+    # print_map(expanded)
+    colour = paint(expanded)
+    print_map(shrink_map(colour))
+    count = sum(r.count(".") for r in shrink_map(colour))
+    return count
 
 if __name__ == "__main__":
-    aoc.run_script(part2)
+    result = aoc.run_script(part2)
+    print(result)
--- day10/test_part2.py
+++ day10/test_part2.py
@@ -1,6 +1,69 @@
 from part2 import *
 import pytest
 
+from test_part1 import simple, example
 
-def test():
-    pass
+
+example_dirty = [
+    "7LF7-",
+    "-FJ|7",
+    "SJ-L7",
+    "|F--J",
+    "LJ-LF",
+]
+
+
+@pytest.mark.parametrize(
+    "dirty, clean", [(simple, simple), (example, example), (example_dirty, example)]
+)
+def test_scrub_map(dirty, clean):
+    route = trace_route(dirty)
+    assert clean == scrub_map(dirty, route)
+
+
+simple_specified = [
+    ".....",
+    ".F-7.",
+    ".|.|.",
+    ".L-J.",
+    ".....",
+]
+
+
+def test_specify_start():
+    specified = specify_start(simple)
+    assert simple_specified == specified
+
+
+simple_expanded = [
+    "..." "..." "..." "..." "...",
+    "..." "..." "..." "..." "...",
+    "..." "..." "..." "..." "...",
+    "..." "..." "..." "..." "...",
+    "..." ".F-" "---" "-7." "...",
+    "..." ".|." "..." ".|." "...",
+    "..." ".|." "..." ".|." "...",
+    "..." ".|." "..." ".|." "...",
+    "..." ".|." "..." ".|." "...",
+    "..." ".|." "..." ".|." "...",
+    "..." ".L-" "---" "-J." "...",
+    "..." "..." "..." "..." "...",
+    "..." "..." "..." "..." "...",
+    "..." "..." "..." "..." "...",
+    "..." "..." "..." "..." "...",
+]
+
+
+def test_expand():
+    expanded = expand_map(simple_specified)
+    assert len(expanded) == 3 * len(simple_specified)
+    assert len(expanded) == len(simple_expanded)
+    assert all(len(r) == 15 for r in expanded)
+    for expected, received in zip(simple_expanded, expanded):
+        assert expected == received
+
+
+def test_shrink():
+    shrunk = shrink_map(simple_expanded)
+    for expected, received in zip(simple_specified, shrunk):
+        assert expected == received
