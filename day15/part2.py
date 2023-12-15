0261e4a37b7d5f3603bf2ae15a89b791d8d74afe
Ian Glover <ian.glover@gmail.com>
2023-12-15 06:38:03 +0000

    Day 15 Part 2

--- day15/part2.py
+++ day15/part2.py
@@ -1,6 +1,7 @@
 import aoc
-import day15.part1 as part1
+from day15.part1 import holiday_hash
 
+import collections
 import functools
 import itertools
 import math
@@ -8,8 +9,34 @@ import numpy
 import re
 
 
+def print_hash_map(hm):
+    print("Hash Map: {")
+    for i, m in enumerate(hm):
+        if m:
+            print("  ", i, m)
+    print("}")
+
+
 def part2(input: list[str]) -> int:
-    pass
+    line = input[0]
+    hash_map = [collections.OrderedDict() for _ in range(256)]
+    for instruction in line.split(","):
+        # print(instruction)
+        m = re.match(r"(\w+)([-]|[=](\d+))", instruction)
+        label, operation, value = m.groups()
+        label_hash = holiday_hash(label)
+        if operation == "-":
+            if label in hash_map[label_hash]:
+                del hash_map[label_hash][label]
+        else:
+            assert operation.startswith("=") and value is not None
+            hash_map[label_hash][label] = int(value)
+        # print_hash_map(hash_map)
+
+    return sum(
+        (box_index + 1) * sum((i + 1) * v for i, v in enumerate(box.values()))
+        for box_index, box in enumerate(hash_map)
+    )
 
 
 if __name__ == "__main__":
