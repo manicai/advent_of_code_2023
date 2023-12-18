0ef1e3302db91debc7b54848be35efa726de3d0b
Ian Glover <ian.glover@gmail.com>
2023-12-18 17:04:17 +0000

    Better explanation of solution for Day 18 part 2

--- day18/part2.py
+++ day18/part2.py
@@ -51,17 +51,23 @@ def shoe_lace(locations):
 
 
 def part2(data: list[str]) -> int:
-    route, length = trace_route((0, 0), data, parse_line)
-    print(f"Length: {length}")
-    # route.append((0, 0))
+    # The (0.5, 0.5) is an arbitrary offset that puts the line of the route
+    # in the middle of all the blocks it passes through. We don't need this
+    # but it makes it easier to think about what's going on.
+    route, length = trace_route((0.5, 0.5), data, parse_line)
     assert route[0] == route[-1], f"Closed loop {route[0]} != {route[-1]}"
     print("Traced route.", flush=True)
-    # This will count the main interior area but not account for
-    # the fact we're using blocks not points.
+    # Calculate the area inside the route. The route line is going down
+    # the middle of the blocks so we are leaving off the parts of the block
+    # outside.
     area = shoe_lace(list(reversed(route)))
-    # To adjust to blocks add half the length of the perimeter as
-    # on average the block is half in half out. Plus one for the
-    # starting block.
+    # Now we need to add the area of the blocks that are outside the route
+    # i.e. all the ones on the perimeter. If going clockwise for right turns
+    # we will so far only have counted a quarter of the turn block but for
+    # left turns we will have counted three quarters. Straights are a half,
+    # fairly obviously. Since it is a closed loop the of right and left turns
+    # cancels out so we can just add half the length. Then need to add one
+    # more for the starting block.
     area += length / 2 + 1
     assert area == int(area), f"Area {area} is not an integer"
     return int(area)
