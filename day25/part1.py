143d28d29d2f25829b59e6d82abe57e20d714d7d
Ian Glover <ian.glover@gmail.com>
2023-12-25 23:21:01 +0000

    Day 25 Part 1

--- day25/part1.py
+++ day25/part1.py
@@ -11,9 +11,48 @@ import numpy
 
 import aoc
 
+LEFT = ["xzz", "vkd", "xxq"]
+RIGHT = ["kgl", "qfb", "hqq"]
+
+
+def convert_to_graphvix(data):
+    for line in data:
+        prefix, suffix = line.split(":")
+        for target in suffix.strip().split(" "):
+            print(f"{prefix} -> {target}")
+
+
+def reachable(neighbors, start):
+    visited = set()
+    queue = [start]
+    while queue:
+        node = queue.pop()
+        if node in visited:
+            continue
+        visited.add(node)
+        queue.extend(neighbors[node])
+    return visited
+
 
 def part1(data: list[str]) -> int:
-    pass
+    neighbors = collections.defaultdict(set)
+    for line in data:
+        prefix, suffix = line.split(":")
+        for target in suffix.strip().split(" "):
+            # Nodes to cut found my
+            if prefix in LEFT and target in RIGHT:
+                print(f"{prefix} -> {target}")
+                continue
+            neighbors[prefix].add(target)
+            neighbors[target].add(prefix)
+
+    print("Size = ", len(neighbors))
+
+    left = reachable(neighbors, LEFT[0])
+    right = reachable(neighbors, RIGHT[0])
+    assert len(left) + len(right) == len(neighbors)
+
+    return len(left) * len(right)
 
 
 if __name__ == "__main__":
