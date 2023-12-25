ac741f4474627b587e7ac8fdb7d95a6d92c087b4
Ian Glover <ian.glover@gmail.com>
2023-12-25 12:07:22 +0000

    Day 24 Part 1

--- day24/part1.py
+++ day24/part1.py
@@ -12,8 +12,51 @@ import numpy
 import aoc
 
 
+def in_zone(x, y):
+    # bound_min, bound_max = 7, 27
+    bound_min, bound_max = 200000000000000, 400000000000000
+    return bound_min <= x <= bound_max and bound_min <= y <= bound_max
+
+
+Particle = collections.namedtuple("Particle", ["position", "velocity"])
+
+
+def parse_line(line):
+    position, velocity = line.split("@")
+    position = tuple(map(int, position.split(",")))
+    velocity = tuple(map(int, velocity.split(",")))
+    return Particle(position, velocity)
+
+
 def part1(data: list[str]) -> int:
-    pass
+    particles = map(parse_line, data)
+
+    count = 0
+    for p, q in itertools.combinations(particles, 2):
+        (p_x, p_y, _), (v_x, v_y, _) = p
+        (q_x, q_y, _), (u_x, u_y, _) = q
+
+        # print(f"Checking {p}, {q}")
+        det = v_x * u_y - v_y * u_x
+        if det == 0:
+            # print(f"Parallel")
+            continue
+
+        t_q = (v_x * (p_y - q_y) - v_y * (p_x - q_x)) / det
+        t_p = (u_x * (p_y - q_y) - u_y * (p_x - q_x)) / det
+        if t_q < 0 or t_p < 0:
+            # print(f"Crossed in past")
+            continue
+
+        x, y = (p_x + t_p * v_x, p_y + t_p * v_y)
+        if in_zone(x, y):
+            # print(f"Collision at {x}, {y}")
+            count += 1
+        else:
+            # print("Collision outside zone")
+            continue
+
+    return count
 
 
 if __name__ == "__main__":
