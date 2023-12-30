a08e9a5d1fe036c08a505ab3a0837aa375466c02
Ian Glover <ian.glover@gmail.com>
2023-12-30 08:34:21 +0000

    Fix unit test day 25.

--- day25/test_part1.py
+++ day25/test_part1.py
@@ -4,12 +4,17 @@ from day25.part1 import *
 
 def test_min_cut():
     edges = []
-    for i in range(5):
-        for j in range(i + 1, 5):
+    partition_size = 5
+    for i in range(partition_size):
+        for j in range(i + 1, partition_size):
             edges.append((f"A{i}", f"A{j}"))
             edges.append((f"B{i}", f"B{j}"))
     edges.append(("A0", "B0"))
     edges.append(("A1", "B1"))
 
-    cut = repeat_karger(edges)
+    cut, partition = repeat_karger(edges, n=500, target_size=2)
     assert len(cut) == 2
+    assert ("A0", "B0") in cut
+    assert ("A1", "B1") in cut
+    assert {f"A{i}" for i in range(partition_size)} in partition
+    assert {f"B{i}" for i in range(partition_size)} in partition
